46d338367eefbc75823d87a000a4f39a179f18d1
root <root@netwrkspider@netwrkspider.org>
2016-04-10 13:39:33 +0530

    script updated and removed ajenti startup

--- config/startrouter.sh
+++ config/startrouter.sh
@@ -1,13 +1,22 @@
 #!/bin/bash
 
-/etc/init.d/networking restart
+echo "++++++++++++++++++++++++++++++++++++++++++"
+echo "+  cryptolink wireless starter script    +"
+echo "++++++++++++++++++++++++++++++++++++++++++"
+
+
+/etc/init.d/networking restart > /dev/null 2>&1
 
 iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
 iptables -A FORWARD -i eth0 -o wlan0 -m state --state RELATED,ESTABLISHED -j ACCEPT
 iptables -A FORWARD -i wlan0 -o eth0 -j ACCEPT
 
-service dnsmasq restart
-service hostapd restart
-service ajenti  restart
+service dnsmasq restart > /dev/null 2>&1
+service hostapd restart > /dev/null 2>&1
 
-banner router started
+service tor stop > /dev/null 2>&1 
+service motion stop > /dev/null 2>&1
+service lighttpd start > /dev/null 2>&1
+service polipo stop > /dev/null 2>&1
+
+#banner smokelink started
